fbcc5578674025ff935c5499881c62942237078d
AmitxParmar <amitparmar901@gmail.com>
2023-01-06 00:57:04 +0530

    create navbar structure

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,12 +1,18 @@
 import './Nav.css';
 
 import React from 'react';
-import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineMessage, AiOutlineUser } from 'react-icons/ai';
+import { BsBriefcase } from 'react-icons/bs';
+import { RiServiceFill } from 'react-icons/ri';
 
 const Nav = () => {
     return (
         <nav>
             <a href='#home'><AiOutlineHome /></a>
+            <a href='#about'><AiOutlineUser /></a>
+            <a href='#experience'><BsBriefcase /></a>
+            <a href='#services'><RiServiceFill /></a>
+            <a href='#contact'><AiOutlineMessage /></a>
         </nav>
     );
 };
