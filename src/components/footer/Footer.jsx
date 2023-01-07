36bb3a35ac8b159131955fbe13d925881813e833
AmitxParmar <amitparmar901@gmail.com>
2023-01-07 23:41:58 +0530

    finish contact and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './Footer.css';
+
 import React from 'react';
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { IoLogoTwitter } from 'react-icons/io';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href='#' className='footer__logo'>AmitxParmar</a>
+
+            <ul className='permalinks'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#experience'>Experience</a></li>
+                <li><a href='#portfolio'>Portfolio</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+
+            <div className='footer__socials'>
+                <a href='https://facebook.com'><FaFacebookF /></a>
+                <a href='https://instagram.com/amitxxparmar'><FaInstagram /> </a>
+                <a href='twitter.com'><IoLogoTwitter /></a>
+            </div>
+
+            <div className='footer__copyright'>
+                <small>&copy; Amit Parmar. All rights reserved.</small>
+            </div>
+
+        </footer>
     );
 };
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,17 +4,19 @@ import './Nav.css';
 import { useState } from 'react';
 import { AiOutlineHome, AiOutlineMessage, AiOutlineUser } from 'react-icons/ai';
 import { BsBriefcase } from 'react-icons/bs';
+import { RiBrushLine } from 'react-icons/ri';
 
 const Nav = () => {
     const [activeNav, setActiveNav] = useState('#');
+
     return (
         <nav>
-            <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? "active" : ''}><AiOutlineHome /></a>
-            <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? "active" : ''}><AiOutlineUser /></a>
-            <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? "active" : ''}><BsBriefcase /></a>
-            <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? "active" : ''}><BsBriefcase /></a>
+            <a title='Home' href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? "active" : ''}><AiOutlineHome /></a>
+            <a title='about' href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? "active" : ''}><AiOutlineUser /></a>
+            <a title='Experience' href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? "active" : ''}><BsBriefcase /></a>
+            <a title='Projects' href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? "active" : ''}><RiBrushLine /></a>
+            <a title='Contact Me' href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? "active" : ''}><AiOutlineMessage /></a>
             {/* <=========commented out for now might add later ====> <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? "active" : ''}><RiServiceFill /></a> */}
-            <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? "active" : ''}><AiOutlineMessage /></a>
         </nav>
     );
 };
